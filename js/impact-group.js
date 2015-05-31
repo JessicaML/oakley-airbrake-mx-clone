e1936a9761c3d1729daf9b4fe487d0b2a1fee487
rlowes <richard.lowes@hotmail.co.uk>
2015-05-31 21:47:15 +0100

    again

--- js/impact-group.js
+++ js/impact-group.js
@@ -6,6 +6,9 @@ var app = app || {};
     app.ImpactGroup = function (elementId) {
         app.BaseGroup.call(this, elementId);
         this.goggles = document.querySelectorAll('.goggles');
+        this.rock = this.el.querySelector('.group3__impact-rock');
+        this.impactData = this.el.querySelector('.group3__impact-data');
+
         this.bg = document.querySelector('#impact-bg');
         this.createStarts();
     };
@@ -17,6 +20,7 @@ var app = app || {};
             // A little workaround for the chrome background-attachment/position relative bug
             this.el.style.visibility = 'visible';
             this.swapImage();
+            this.pushRock();
         } else {
             this.el.style.visibility = 'hidden';
         }
@@ -43,6 +47,32 @@ var app = app || {};
             } else {
                 goggle.style.display = 'none';
             }      
+        }
+    };
+
+    /**
+     * Complete mess, fix it!
+     */
+    app.ImpactGroup.prototype.pushRock = function () {
+        var vp = this.getViewport(),
+            needle = (vp.top - window.innerHeight);
+        //console.log(needle);
+        if (needle > 600 && needle < 800) {
+            var left = (needle - 600) / 2;
+            var opacity = (needle - 600) / 100;
+            this.translate(this.rock, left, 0);
+            this.rock.style.opacity = opacity;
+        }
+
+        if(needle > 700 && needle < 800) {
+            var opacity = (needle - 700) / 100;
+            console.log('op', opacity);
+            this.impactData.style.opacity = opacity;
+        }
+
+        if(needle >= 900) {
+            this.impactData.style.opacity = 0;
+            this.rock.style.opacity = 0;
         }
     };
 
